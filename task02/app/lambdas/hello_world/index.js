6ea0df64c2fd56e86274e9545d228bd93e038d8f
Nazar Montsibovych <nazar_montsibovych@epam.com>
2025-03-18 11:40:26 +0200

    task02 fix

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,29 +1,36 @@
 exports.handler = async (event) => {
     console.log(event, 'event');
     const requestedContext = event?.requestContext;
+    console.log(requestedContext)
 
     if(requestedContext) {
         const { http } = requestedContext;
 
         if(http.method === 'GET' && http.path === '/hello') {
-            return ({
+            return {
                 statusCode: 200,
-                body: 'Hello from Lambda',
-            })
+                body: JSON.stringify({
+                    statusCode: 200,
+                    message: 'Hello from Lambda'
+                })
+            }
         } else {
-            return (
-                {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
                     statusCode: 400,
                     message: `Bad request syntax or unsupported method. Request path: ${http.path}. HTTP method: ${http.method}`
-                }
-            )
+                })
+            }
+            
         }
     }
 
-    return (
-        {
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
             statusCode: 400,
             message: 'Bad request syntax or unsupported method'
-        }
-    )
+        })
+    }
 };

feeb70a0141b9fcf45bd07fb3100774f78b0e6d7
Nazar Montsibovych <nazar_montsibovych@epam.com>
2025-03-30 17:33:37 +0300

    task04

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,8 +1,15 @@
+const AWS = require("aws-sdk");
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  console.log("Received SNS message:");
+  console.log(JSON.stringify(event, null, 2));
+
+  // Access specific parts of the SNS message, if needed
+  if (event.Records && event.Records.length > 0) {
+    const snsMessage = event.Records[0].Sns;
+    console.log("Message Subject:");
+    console.log(snsMessage.Subject);
+    console.log("Message Body:");
+    console.log(snsMessage.Message);
+  }
 };
--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,9 +1,13 @@
+const AWS = require("aws-sdk");
+
 exports.handler = async (event) => {
-  // TODO implement
-  console.log("111");
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
-  };
-  return response;
+  console.log("Received SQS message:");
+  console.log(JSON.stringify(event, null, 2));
+
+  // Access specific parts of the SQS message, if needed
+  if (event.Records && event.Records.length > 0) {
+    const sqsMessage = event.Records[0];
+    console.log("Message Body:");
+    console.log(sqsMessage.body);
+  }
 };

4481608f650ddc11ab4faaf5fb01d41e8afe22dc
Nazar Montsibovych <nazar_montsibovych@epam.com>
2025-03-30 18:58:20 +0300

    task04 change code

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,13 +1,4 @@
-const AWS = require("aws-sdk");
-
 exports.handler = async (event) => {
-  console.log("Received SQS message:");
-  console.log(JSON.stringify(event, null, 2));
-
-  // Access specific parts of the SQS message, if needed
-  if (event.Records && event.Records.length > 0) {
-    const sqsMessage = event.Records[0];
-    console.log("Message Body:");
-    console.log(sqsMessage.body);
-  }
+    console.log('Received SQS message:');
+    console.log(JSON.stringify(event.Records[0].body, null, 2));
 };
